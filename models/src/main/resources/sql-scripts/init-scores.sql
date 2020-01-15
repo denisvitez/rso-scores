0cc0de596bf1a1728b02332a3f64a7eba460fb12
Denis Vitez <denis.vitez@gmail.com>
2020-01-15 23:26:06 +0100

    Updated initial script.

--- models/src/main/resources/sql-scripts/init-scores.sql
+++ models/src/main/resources/sql-scripts/init-scores.sql
@@ -1,3 +1,3 @@
-INSERT INTO scores (value, comment, date_inserted) VALUES (1.0, 'Bad beer', TIMESTAMP '2019-11-30 12:00:00');
-INSERT INTO scores (value, comment, date_inserted) VALUES (3.6, 'Not great, not terrible', TIMESTAMP '2019-11-30 12:05:00');
-INSERT INTO scores (value, comment, date_inserted) VALUES (5.0, 'Amazing', TIMESTAMP '2019-11-30 12:10:00');
+INSERT INTO scores (value, comment, beerId, date_inserted) VALUES (1.0, 'Bad beer', 1, TIMESTAMP '2019-11-30 12:00:00');
+INSERT INTO scores (value, comment, beerId, date_inserted) VALUES (3.6, 'Not great, not terrible', 2, TIMESTAMP '2019-11-30 12:05:00');
+INSERT INTO scores (value, comment, beerId, date_inserted) VALUES (5.0, 'Amazing', 3, TIMESTAMP '2019-11-30 12:10:00');
